f8506f5ed023362600664f64036fac13072e40f3
whitestrannik <strannik.work@outlook.com>
2016-04-13 21:40:49 +0300

    Added new project for collection' experiments and cons calls sequence

---
+++ Glab.Base.Collection/DynamicArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glab.Base.Collection
+{
+    public class DynamicArray<T> : IEnumerable<T>, IEnumerable
+    {
+        public DynamicArray()
+        {
+            _elementCount = 0;
+            _length = 7;
+            _array = new T[_length];
+        }
+
+        public DynamicArray(IEnumerable<T> array)
+        {
+            _length = GetEnumerableLength(array);
+            _elementCount = _length;            
+            _array = new T[_length];
+
+            int index = 0;
+            foreach (var item in array)
+                _array[++index] = item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #region private
+
+        T[] _array;
+        int _elementCount;
+        int _length;
+
+        private int GetEnumerableLength(IEnumerable array)
+        {
+            if (array == null) return 0;
+
+            int count = 0;
+            foreach (var item in array)
+                count++;
+
+            return count;
+        }
+
+        #endregion
+    }
+}
---
+++ Glab.Test.Misc/CollectionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using Glab.Base.Collection;
+using System.Collections.Generic;
+
+namespace Glab.Test.Misc
+{
+    [TestClass]
+    public class CollectionTest
+    {
+        [TestMethod]
+        public void DynamicArrayTest()
+        {
+            IEnumerable<Tuple<object>> array = new DynamicArray<Tuple<object>>();
+            foreach (var item in array)
+                Assert.Fail("Empty array can not be iterrate");
+
+            List<Tuple<object>> source = new List<Tuple<object>> { new Tuple<object>("str1"), new Tuple<object>("str2") };
+            array = new DynamicArray<Tuple<object>>(source);
+
+            var count = 0;
+            foreach (var item in array)
+                Assert.AreSame(item, source[++count], "The input source and result array' element are different");
+
+            Assert.AreEqual<int>(source.Count, count, "Size of input source and result array is different");
+        }
+    }
+}
