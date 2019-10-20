dec59c849dfb7e8aeb206e693653f851fd3ba45b
Mikhail Bazhanov <mbazhanov@newgistics.com>
2019-10-20 16:05:23 +0300

    Heap, primitives, tests etc.

---
+++ Glab.Base.Collection/Heap.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Glab.Base.Collection
+{
+    public class Heap<T>
+    {
+        private T[] _array;
+        private int _count;
+        private readonly IComparer<T> _comparer;
+
+        public Heap(T[] input, int size, IComparer<T> comparer)
+        {
+            var arraySize = Math.Max(input == null? 0 : input.Length, size);
+
+            _array = new T[arraySize];
+
+            _comparer = comparer;
+
+            foreach (var item in input)
+            {
+                Add(item);
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (_count == _array.Length)
+            {
+                var newArray = new T[2 * _array.Length];
+                Array.Copy(_array, newArray, _array.Length);
+                _array = newArray;
+            }
+
+            AddInternal(value);
+        }
+
+        public bool IsEmpty => _count == 0;
+
+        public T GetExtremum()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Heap is empty");
+            }
+
+            return _array[0];
+        }
+
+        public T GetAndRemoveExtremum()
+        {
+            var extremum = GetExtremum();
+
+            _array[0] = _array[_count - 1];
+            _count--;
+
+            var index = 0;
+            while (true)
+            {
+                var nextElementIndex = GetMaxChildIndex(index);
+                if (nextElementIndex == -1 || _comparer.Compare(_array[nextElementIndex], _array[index]) < 0)
+                {
+                    break;
+                }
+                else
+                {
+                    SwapValues(index, nextElementIndex);
+                    index = nextElementIndex;
+                }
+            }
+
+            return extremum;
+        }
+
+        private int GetMaxChildIndex(int index)
+        {
+            var leftIndex = GetLeftChildIndex(index);
+            var rightIndex = GetRightChildIndex(index);
+            if (leftIndex >= _count)
+            {
+                return -1;
+            }
+            else if (leftIndex < _count && rightIndex >= _count)
+            {
+                return leftIndex;
+            }
+            else
+            {
+                return _comparer.Compare(_array[leftIndex], _array[rightIndex]) > 0 ? leftIndex : rightIndex;
+            }
+        }
+
+
+        private void AddInternal(T value)
+        {
+            var index = _count;
+            _array[index] = value;
+            _count++;
+
+            var parentIndex = GetParentIndex(index);
+            while (parentIndex >= 0 && _comparer.Compare(value, _array[parentIndex]) > 0)
+            {
+                SwapValues(parentIndex, index);
+                index = parentIndex;
+                parentIndex = GetParentIndex(index);
+            }
+        }
+
+        private void SwapValues(int parentIndex, int index)
+        {
+            var temp = _array[parentIndex];
+            _array[parentIndex] = _array[index];
+            _array[index] = temp;
+        }
+
+        private int GetParentIndex(int childIndex)
+        {
+            if (childIndex == 0)
+            {
+                return -1;
+            }
+
+            return (childIndex - 1) / 2;
+        }
+
+        private int GetLeftChildIndex(int parentIndex)
+        {
+            return parentIndex * 2 + 1;
+        }
+
+        private int GetRightChildIndex(int parentIndex)
+        {
+            return parentIndex * 2 + 2;
+        }
+
+        private T GetParentValue(int childIndex)
+        {
+            return _array[(childIndex - 1) / 2];
+        }
+
+        private T GetLeftChildValue(int parentIndex)
+        {
+            return _array[parentIndex * 2 + 1];
+        }
+
+        private T GetRightChildValue(int parentIndex)
+        {
+            return _array[parentIndex * 2 + 2];
+        }
+    }
+}
---
+++ Glab.Test.Base/UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Glab.Base.Collection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Glab.Test.Base
+{
+    [TestClass]
+    public class HeapTest
+    {
+        [TestMethod]
+        public void HeapMinTest_AddAndGetExtremum()
+        {
+            var input = new int[] { 8, 5, 3, 3, 7, 11, 6, 0};
+            Heap<int> heap = new Heap<int>(input, 5, new HeapMinComparator());
+            Assert.AreEqual(0, heap.GetExtremum());
+
+            heap.Add(0);
+            Assert.AreEqual(0, heap.GetExtremum());
+
+            heap.Add(2);
+            Assert.AreEqual(0, heap.GetExtremum());
+
+            heap.Add(-2);
+            Assert.AreEqual(-2, heap.GetExtremum());
+
+            heap.Add(-1);
+            Assert.AreEqual(-2, heap.GetExtremum());
+
+            heap.Add(-2);
+            Assert.AreEqual(-2, heap.GetExtremum());
+
+            heap.Add(1000);
+            Assert.AreEqual(-2, heap.GetExtremum());
+
+            heap.Add(-5);
+            Assert.AreEqual(-5, heap.GetExtremum());
+        }
+
+        [TestMethod]
+        public void HeapMinTest_GetAndRemoveExtremum()
+        {
+            var input = new int[] { 8, 5, 3, 3, 7, 2, 11, 6, 0, 1, 7 };
+            Heap<int> heap = new Heap<int>(input, 5, new HeapMinComparator());
+            Assert.AreEqual(0, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(1, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(2, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(3, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(3, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(5, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(6, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(7, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(7, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(8, heap.GetAndRemoveExtremum());
+            Assert.AreEqual(11, heap.GetAndRemoveExtremum());
+            Assert.IsTrue(heap.IsEmpty);
+        }
+
+        public class HeapMinComparator : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                if (x == y)
+                {
+                    return 0;
+                }
+                else if (x > y)
+                {
+                    return -1;
+                }
+
+                return 1;
+            }
+        }
+    }
+}
